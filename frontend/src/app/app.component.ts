2613d7e1fead00716af49b4723d32a0bd516c607
nloewes <geilepaste@proton.me>
2023-04-04 19:45:13 +0200

    Use dynamic icons for game result, add baseline usable UI

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 
-import {faHandScissors as faScissors, IconDefinition} from '@fortawesome/free-solid-svg-icons';
+import {faHandScissors as faScissors, faHandRock as faRock, faHandPaper as faPaper, IconDefinition, faQuestion as faQuestion} from '@fortawesome/free-solid-svg-icons';
 
 import { GameResultService } from '../../openapi-test/build/services/game-result.service';
 import { GameResult } from '../../openapi-test/build/models/game-result';
@@ -16,59 +16,92 @@ import { PlayerTurn } from 'openapi-test/build/models';
 })
 export class GameComponent implements OnInit {
     title = 'RoshambrFrontend';
-
     PlayerChoice = PlayerChoice;
 
     playerChoice: PlayerChoice | undefined;
+    playerSelection: PlayerChoice | undefined;
+
     computerChoice: PlayerChoice | undefined;
+    computerSelection: PlayerChoice | undefined;
+
     result: MatchResult | undefined;
-    aiResult: GameResult | undefined;
     isLoading = false;
+
+    playerScore = 0;
+    apiScore = 0;
+    turnResult: string | undefined;
+
+    playerIcon = faQuestion;
+    apiIcon = faQuestion;
 
     constructor(private http: HttpClient, private gameResultService: GameResultService) { }
 
     ngOnInit(): void {
     }
 
+    /**
+     * Main function to play a simple game of rock-paper-scissors against an API and update the properties of this component accordingly
+     * @param playerChoice a PlayerChoice to use for the round of RPS
+     */
     playGame(playerChoice: PlayerChoice): void {
         this.isLoading = true;
         this.playerChoice = playerChoice;
 
-        /* this.playPost(playerChoice)
-        .subscribe((response : ApiGameResult) => {
-            this.computerChoice = this.getRandomChoice();
-            this.result = response.result;
-            this.isLoading = false;
-
-            this.apiResult = response;
-            this.playerChoice = this.apiResult.playerChoice;
-            this.computerChoice = this.apiResult.apiChoice;
-            this.result = this.apiResult.result;
-            this.isLoading = false;
-        }); */
-
         this.playApi(playerChoice).subscribe((response: GameResult) => {
             this.result = response.result;
             this.computerChoice = response.player2Choice;
+            this.processResult(response);
 
-            console.log(this.result);
-            console.log(this.computerChoice);
+            this.playerSelection = this.playerChoice;
+            this.computerSelection = this.computerChoice;
 
+            this.playerChoice = undefined;
+            this.computerChoice = undefined;
             this.isLoading = false;
         });
-
-        /* this.computerChoice = this.getRandomChoice();
-        this.result = playerChoice + " | " + this.computerChoice;
-        console.log(this.result); 
-        this.isLoading = false; */
     }
 
+    /**
+     * Takes a given PlayerChoice and uses it to send a request via the GameResultService
+     * @param choice the choice to use for the request
+     * @returns an Observable of <GameResult>
+     */
     private playApi(choice: PlayerChoice) {
         var turn: PlayerTurn = {playerChoice: choice}
         return this.gameResultService.postMatch({body: turn});
     }
 
+    /**
+     * Processes a given GameResult, updating the associated properties 
+     * @param result the GameResult based on which we want to update
+     */
+    private processResult(result: GameResult): void {
+        if (result.result == MatchResult.Player1Win) {
+            this.playerScore++;
+            this.turnResult = 'YOU WIN!';
+        } else if (result.result == MatchResult.Player2Win) {
+            this.apiScore++;
+            this.turnResult = 'API WINS!';
+        } else {
+            this.turnResult = 'DRAW!';
+        }
+
+        this.playerIcon = this.getIcon(result.player1Choice);
+        this.apiIcon = this.getIcon(result.player2Choice);
+    }   
+
+    /**
+     * Takes a given PlayerChoice and returns the corresponding IconDefinition
+     * @param choice the given PlayerChoice
+     * @returns the IconDefinition associated with the PlayerChoice
+     */
     private getIcon(choice: PlayerChoice): IconDefinition {
-        return faScissors;
+        if (choice == PlayerChoice.Rock) {
+            return faRock;
+        } else if (choice == PlayerChoice.Paper) {
+            return faPaper;
+        } else {
+            return faScissors;
+        }
     }
 }
