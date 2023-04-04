7d17dc6302aa55a600c94c5a9450f6919b66c95c
nloewes <geilepaste@proton.me>
2023-04-04 17:15:26 +0200

    Add basic cors config, update dockerfile and docker-compose

--- backend/src/main/java/de/nloewes/roshambr/config/CorsConfig.java
+++ backend/src/main/java/de/nloewes/roshambr/config/CorsConfig.java
@@ -1,2 +1,22 @@
-package de.nloewes.roshambr.config;public class CorsConfig {
+package de.nloewes.roshambr.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Allow-all cors configuration for demo purposes
+ */
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
 }
+
+
+
